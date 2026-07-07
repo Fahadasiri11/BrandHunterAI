5f2938ed596b748bddd9c04f987ea1103b4a6be5
Fahadasiri11 <tiiman7@hotmail.com>
2026-07-07 21:16:24 +0300

    Integrate domain availability check in dataframe

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
 import pandas as pd
+from domain_checker import check_domain
 
 def names_to_dataframe(names):
     data = []
 
     for name in names:
+
+        result = check_domain(name.lower() + ".com")
+
+        if result["status"] == "Available":
+            domain = "✅"
+        else:
+            domain = "❌"
+
         data.append({
             "Brand": name,
-            ".com": "⏳",
-            ".ai": "⏳",
-            "Risk": "-"
+            ".com": domain
         })
 
     return pd.DataFrame(data)
